07ae897e8e7c2b8b214cdadc934a3eac329f2e75
Hack Reactor Student <student@hackreactor.com>
2018-03-29 15:44:05 -0500

    buggy mvp

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const level = require('level');
+const { updateStore } = require('./updatestore');
 
 const db = level('./mydb');
 
@@ -11,6 +12,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => res.render('main'));
+app.get('/:url', (req, res) => {
+  const { url } = req.params;
+  console.log(typeof url);
+  db.get(url, (err, webpage) => {
+    if (err) {
+      // console.log('error', err);
+    }
+
+    if (webpage === '') {
+      res.render('wait');
+    } else {
+      res.end(webpage);
+    }
+  });
+});
 
 app.post('/', (req, res) => {
   const { url } = req.body;
@@ -23,16 +39,20 @@ app.post('/', (req, res) => {
         if (err) return console.log(err);
       });
       // serve html(value)
-      res.end('please wait');
+      res.redirect(`/${url}`);
     } else {
       if (value) {
+        res.redirect(`/${url}`);
         res.end(value);
       } else {
-        res.end('please wait');
+        res.redirect(`/${url}`);
+
         // serve html(value)
       }
     }
   });
 });
+
+setInterval(() => updateStore(db), 30000);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- updatestore.js
+++ updatestore.js
@@ -1,20 +1,22 @@
 const request = require('request');
 const level = require('level');
 
-const db = level('./mydb');
-
 exports.updateStore = db => {
   db
     .createReadStream()
     .on('data', function(data) {
       if (!data.value) {
         request(`http://${data.key}`, (err, res, body) => {
-          if (res.statusCode === 200) {
-            db.put(data.key, body);
+          if (err) {
+            console.log(err);
+            db.del(data.key);
+          } else {
+            if (res.statusCode === 200) {
+              db.put(data.key, body);
+            }
           }
         });
       }
-      console.log(data.key, '=', data.value);
     })
     .on('error', function(err) {
       console.log('Oh my!', err);
@@ -26,5 +28,3 @@ exports.updateStore = db => {
       console.log('Stream ended');
     });
 };
-
-exports.updateStore(db);
